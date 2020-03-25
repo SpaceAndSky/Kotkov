f78277d44ff372fd5d459eca8fcd02daced6c591
SpaceAndSky <spaceandsky@yandex.ru>
2020-03-25 20:06:02 +0300

    comment

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 25 2020 г., 18:28
+-- Время создания: Мар 25 2020 г., 20:02
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -32,8 +32,8 @@ CREATE TABLE `bets` (
   `id` smallint(6) NOT NULL,
   `create_ts` int(11) NOT NULL,
   `price` mediumint(9) NOT NULL,
-  `lot_id` smallint(6) DEFAULT NULL,
-  `user_id` smallint(6) DEFAULT NULL
+  `lot_id` smallint(6) NOT NULL,
+  `user_id` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -57,13 +57,13 @@ CREATE TABLE `lots` (
   `id` smallint(6) NOT NULL,
   `name` char(128) NOT NULL,
   `description` text NOT NULL,
-  `price` mediumint(9) NOT NULL,
+  `price` mediumint(9) DEFAULT NULL,
   `step` smallint(6) NOT NULL,
   `create_ts` int(11) NOT NULL,
   `expire_ts` bigint(20) NOT NULL,
   `img` char(128) NOT NULL,
-  `category_id` char(10) DEFAULT NULL,
-  `author` smallint(6) DEFAULT NULL,
+  `category_id` char(10) NOT NULL,
+  `author` smallint(6) NOT NULL,
   `winner_id` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -80,8 +80,7 @@ CREATE TABLE `users` (
   `password_hash` char(60) NOT NULL,
   `contacts` char(255) NOT NULL,
   `registration_date` datetime NOT NULL,
-  `img` char(128) NOT NULL,
-  `id_of_lots` smallint(6) DEFAULT NULL
+  `img` char(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -93,7 +92,6 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `bets`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
   ADD KEY `price` (`price`),
   ADD KEY `lot_id` (`lot_id`),
   ADD KEY `user_id` (`user_id`);
@@ -102,31 +100,25 @@ ALTER TABLE `bets`
 -- Индексы таблицы `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `lots`
 --
 ALTER TABLE `lots`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
   ADD KEY `price` (`price`),
   ADD KEY `step` (`create_ts`),
-  ADD KEY `expire_ts` (`expire_ts`),
   ADD KEY `category_id` (`category_id`),
   ADD KEY `user_id` (`author`),
   ADD KEY `winner_id` (`winner_id`);
-ALTER TABLE `lots` ADD FULLTEXT KEY `lot_search` (`name`,`description`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `id_of_lots` (`id_of_lots`);
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -158,22 +150,15 @@ ALTER TABLE `users`
 -- Ограничения внешнего ключа таблицы `bets`
 --
 ALTER TABLE `bets`
-  ADD CONSTRAINT `bets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `bets_ibfk_2` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `bets_ibfk_1` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `lots`
 --
 ALTER TABLE `lots`
-  ADD CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lots_ibfk_2` FOREIGN KEY (`author`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ограничения внешнего ключа таблицы `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_of_lots`) REFERENCES `lots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
